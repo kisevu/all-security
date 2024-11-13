f0a11af320c5b385f445fef19292e106a40d8349
ameda-kevin <amedakevin@gmail.com>
2024-11-13 18:39:14 +0300

    role based authorization, method level security enabled using annotations

--- jwt/src/main/java/com/kisevu/works/security/config/WebSecurityConfig.java
+++ jwt/src/main/java/com/kisevu/works/security/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ package com.kisevu.works.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
     @Bean
--- jwt/src/main/java/com/kisevu/works/security/controller/GreetingsController.java
+++ jwt/src/main/java/com/kisevu/works/security/controller/GreetingsController.java
@@ -6,6 +6,7 @@ package com.kisevu.works.security.controller;
 *
 */
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingsController {
 
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin(){
+        return "Hello Admin";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String user(){
+        return "Hello User";
     }
 }

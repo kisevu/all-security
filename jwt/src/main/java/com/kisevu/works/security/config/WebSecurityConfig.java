9cf99a2f9267f8480ab9be5634da1a49f4328938
ameda-kevin <amedakevin@gmail.com>
2024-11-13 17:58:21 +0300

    form login and basic

--- jwt/src/main/java/com/kisevu/works/security/config/WebSecurityConfig.java
+++ jwt/src/main/java/com/kisevu/works/security/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +21,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(authorizeRequests ->authorizeRequests.anyRequest().authenticated());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
        return  http.build();
